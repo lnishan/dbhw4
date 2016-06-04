92d8fa9e5254cb76881f227c55a8abd178afbcf3
Jasmine Chen <j.lin013@gmail.com>
2016-06-04 03:42:37 +0000

    Update makefile and run script

--- makefile
+++ makefile
@@ -3,7 +3,7 @@ all: test
 test: main.cpp db.cpp db.h
 	g++ -std=c++11 main.cpp db.cpp -o test
 
-O2:
+test_O2:
 	g++ -std=c++11 -O2 main.cpp db.cpp -o test_O2
 
 clean:
--- run.sh
+++ run.sh
@@ -1,11 +1,67 @@
 #! /bin/sh
 
 make
-make O2
+make test_O2
 
 if [ "$1" = "O2" ]; then
-	./test_O2; sleep 20; ./test_O2; sleep 20; ./test_O2; sleep 20; ./test_O2; sleep 20; ./test_O2; sleep 20;
+	
+	echo "#1"
+	./test_O2;
+	echo ""
+	
+	sleep 20;
+	
+	echo "#2"
+	./test_O2;
+	echo ""
+	
+	sleep 20;
+
+	echo "#3"
+	./test_O2;
+	echo ""
+	
+	sleep 20;
+
+	echo "#4"
+	./test_O2;
+	echo ""
+	
+	sleep 20;
+
+	echo "#5"
+	./test_O2;
+	echo ""
+
 else
-	./test; sleep 20; ./test; sleep 20; ./test; sleep 20; ./test; sleep 20; ./test; sleep 20;
+	
+	echo "#1"
+	./test;
+	echo ""
+	
+	sleep 20;
+	
+	echo "#2"
+	./test;
+	echo ""
+	
+	sleep 20;
+
+	echo "#3"
+	./test;
+	echo ""
+	
+	sleep 20;
+
+	echo "#4"
+	./test;
+	echo ""
+	
+	sleep 20;
+
+	echo "#5"
+	./test;
+	echo ""
+
 fi
 
