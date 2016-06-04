084b0fc82d0a1eddf02fae82e4fc1b4d31894b26
Jasmine Chen <j.lin013@gmail.com>
2016-06-05 02:48:44 +0800

    Update main.cpp to show time in realtime

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ int main(int argc, char* argv[]){
 	mydb.import("data/2007.csv");
 	mydb.import("data/2008.csv");
 	double import_time = (double)(clock() - tImport) / CLOCKS_PER_SEC;
+	printf("Time taken for import: %.4fs\n", import_time);
 	
 	//Query without index
 	clock_t tNoIdxQuery = clock();
@@ -29,11 +30,13 @@ int main(int argc, char* argv[]){
 	double noidx_result4 = mydb.query("JFK", "IAH");
 	double noidx_result5 = mydb.query("LAX", "IAH");
 	double noidx_query_time = (double)(clock() - tNoIdxQuery) / CLOCKS_PER_SEC;
+	printf("Time taken for indexing: %.4fs\n", index_time);
 	
 	//Create index on one or two columns.
 	clock_t tIndex = clock();
 	mydb.createIndex();
 	double index_time = (double)(clock() - tIndex) / CLOCKS_PER_SEC;
+	printf("Time taken for queries (pre-indexing): %.4fs\n", noidx_query_time);
 
 	//Do queries
 	//These queries are required in your report.
@@ -48,14 +51,11 @@ int main(int argc, char* argv[]){
 
 	//End timing
 	double query_time = (double)(clock() - tQuery) / CLOCKS_PER_SEC;
+	printf("Time taken for queries: %.4fs\n", query_time);
 	
 	printf("Results (pre-indexing): %.4f %.4f %.4f %.4f %.4f\n", noidx_result1, noidx_result2, noidx_result3, noidx_result4, noidx_result5);
 	printf("Results: %.4f %.4f %.4f %.4f %.4f\n", result1, result2, result3, result4, result5);
-	
-	printf("Time taken for import: %.4fs\n", import_time);
-	printf("Time taken for indexing: %.4fs\n", index_time);
-	printf("Time taken for queries (pre-indexing): %.4fs\n", noidx_query_time);
-	printf("Time taken for queries: %.4fs\n", query_time);
+
 
 	//Cleanup db object
 	mydb.cleanup();
