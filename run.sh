5eada37b1abe6e127fc9d3bd8f469bd96ce6baae
Jasmine Chen <j.lin013@gmail.com>
2016-06-04 11:32:17 +0800

    Add run script

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+make
+make O2
+
+if [ "$1" = "O2" ]; then
+	./test_O2; sleep 20; ./test_O2; sleep 20; ./test_O2; sleep 20; ./test_O2; sleep 20; ./test_O2; sleep 20;
+else
+	./test; sleep 20; ./test; sleep 20; ./test; sleep 20; ./test; sleep 20; ./test; sleep 20;
+fi
+
