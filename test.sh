bafbbd51eccd4f542f766a64d2dd62c00b3e903b
Jasmine Chen <j.lin013@gmail.com>
2016-06-15 03:23:19 +0800

    Improve test script

--- test.sh
+++ test.sh
@@ -1,11 +1,40 @@
 #! /bin/sh
 
 if [ -n "$1" ]; then
+	binary="test_$1"
+else
+	binary="test"
+fi
+
+printf "* Building binaries ... "
+make $binary > last_build.log 2>&1
+if [ ! $? -eq 0 ]; then
+	echo "Error"
+	cat last_build.log
+	echo "* An error has occurred! Aborting ..."
+	exit
+else
+	echo "Done"
+fi
+
+
+if [ -z "$2" ]; then
+	echo "* Starting dry-runs ..."
+	printf "1 ... "
+	./$binary > /dev/null 2>&1
+	printf "2 ... "
+	./$binary > /dev/null 2>&1
+	echo "OK"
+fi
+
+
+echo "* Starting test ..."
+if [ -n "$1" ]; then
 	./run.sh $1 | tee res_raw.txt
 else
 	./run.sh | tee res_raw.txt
 fi
 
-make res
+make res > /dev/null 2>&1
 ./res res_raw.txt | tee res.txt
 
